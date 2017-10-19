503c8ca2c29744b5ac4515e6bd82bbda996ad141
Mikel Dean <dean.mikel888@gmail.com>
2017-10-19 12:35:14 +0200

    Lab2b

---
+++ Lab2b/konfigurazioa.php
@@ -0,0 +1,14 @@
+<?php 
+$lokal = 0;
+if($lokal==0){
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "quiz";
+}else{
+	$servername = "localhost";
+	$username = "id2926954_wst18";
+	$password = "wst18";
+	$dbname = "id2926954_quiz";
+}	
+?>
---
+++ Lab2b/showQuestions.php
@@ -0,0 +1,29 @@
+ <?php
+
+include 'konfigurazioa.php';
+
+// Create connection
+$connection = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($connection->connect_error) {
+    die("Konekxioak kale egin: " . $conn->connect_error);
+}
+//echo "Zuzen konektatuta";
+echo "<br/>";
+
+$galderak = $connection->query("SELECT * from questions");
+
+echo '<table border=1> <tr> <th> ID </th> <th> Eposta </th> <th> Galdera </th> <th> Erantzun zuzena </th>'; 
+echo '<th> Erantzun okerra 1 </th> <th> Erantzun okerra 2 </th> <th> Erantzun okerra 3 </th> <th> Zailtasuna </th> <th> Gai-arloa </th> </tr>';
+
+while ( $galdera = $galderak->fetch_object()) {
+		echo '<tr> <td> '.$galdera->ID.' </td> <td> '.$galdera->Eposta.' </td> <td> '.$galdera->Galdera.' </td> <td> '.$galdera->ErantzunZuzena.'</td>';
+        echo '<td> '.$galdera->ErantzunOkerra1.' </td> <td> '.$galdera->ErantzunOkerra2.' </td> <td> '.$galdera->ErantzunOkerra3.' </td>';
+		echo '<td> '.$galdera->Zailtasuna.' </td> <td> '.$galdera->GaiArloa.' </td> </tr>';
+}	
+echo '</table>';
+echo "<a href='layout.html'> Itzuli orri nagusira </a>";
+$galderak->close();
+$connection->close();
+?> 
