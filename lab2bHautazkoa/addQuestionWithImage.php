e6a0b39651339409ef6f976950002cce144c5765
Mikel Dean <dean.mikel888@gmail.com>
2017-10-25 08:17:02 +0200

    lab2bHautazkoa

---
+++ lab2bHautazkoa/addQuestionWithImage.php
@@ -0,0 +1,49 @@
+ <?php
+
+ include 'konfigurazioa.php';
+
+
+// Create connection
+$connection = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($connection->connect_error) {
+    die("Konekxioak kale egin: " . $conn->connect_error);
+}
+//echo "Zuzen konektatuta";
+echo "<br/>";
+
+$eposta = $_POST['eposta'];
+
+$galdera = $_POST['galdera'];
+
+$eranZuzen = $_POST['eranZuzen'];
+
+$eranOker1 = $_POST['eranOker1'];
+
+$eranOker2 = $_POST['eranOker2'];
+
+$eranOker3 = $_POST['eranOker3'];
+
+$zailtasun = $_POST['zailtasun'];
+
+$gaia = $_POST['gaia'];
+
+$irudia = addslashes (file_get_contents($_FILES['gehituIrudia']['tmp_name']));
+
+$sql = "INSERT INTO questions(Eposta, Galdera, ErantzunZuzena, ErantzunOkerra1, ErantzunOkerra2, ErantzunOkerra3, Zailtasuna, GaiArloa, Irudia) VALUES ('$eposta','$galdera','$eranZuzen','$eranOker1','$eranOker2','$eranOker3','$zailtasun','$gaia', '$irudia')";
+
+if ($connection->query($sql) === TRUE) {
+    echo "Datu-basean galdera zuzen txertatu da <br/>";
+	echo "<a href='showQuestionsWithImage.php'> Ikusi datu-baseko galderak </a><br/>";
+	echo "<a href='addQuestion.html'> Beste galdera bat txertatu </a><br/>";
+} else {
+    //echo "Error: " . $sql . "<br>" . $connection->error;
+	echo "Errorea: Galdera datu-basean txertatzea ez da posible izan <br/>";
+	echo "<a href='addQuestion.html'> Beste galdera bat txertatu </a><br/>";
+	
+}
+
+$connection->close();
+
+?> 
---
+++ lab2bHautazkoa/konfigurazioa.php
@@ -0,0 +1,14 @@
+<?php 
+$lokal = 0;
+if($lokal==0){
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "quiz";
+}else{
+	$servername = "localhost";
+	$username = "id2954107_wst18";
+	$password = "";
+	$dbname = "id2954107_quiz";
+}	
+?>
